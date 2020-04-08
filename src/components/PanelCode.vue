73873fcc285c41ff55392a63d0c9e8fb4a8de8e7
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-04-08 19:25:19 -0400

    added ace ide

--- src/components/PanelCode.vue
+++ src/components/PanelCode.vue
@@ -1,12 +1,12 @@
 <template>
     <panel id="panel-code">
-
+        <ace v-model="content" @init="editorInit" lang="javascript" theme="chrome" width="400" height="200"></ace>
     </panel>
 </template>
 
 <style lang="scss">
 #panel-code {
-    --size: 300px;
+    --size: 400px;
     grid-row: 1 / -1;
     grid-column: 1 / 2;
 }
@@ -14,10 +14,27 @@
 
 <script>
 import Panel from "./Panel"
+import Ace from "vue2-ace-editor"
 export default {
     name: "PanelCode",
+    data() {
+        return {
+            content: "avancer(10);\r\nfor (let x = 0; x < 10; x += 1) {\r\ntourner(5);\r\n}",
+        }
+    },
     components: {
-        Panel
+        Ace,
+        Panel,
+    },
+    methods: {
+        editorInit: function () {
+            require('brace/ext/language_tools') //language extension prerequsite...
+            require('brace/mode/html')                
+            require('brace/mode/javascript')    //language
+            require('brace/mode/sass')
+            require('brace/theme/chrome')
+            require('brace/snippets/javascript') //snippet
+        }
     }
 }
 </script>
